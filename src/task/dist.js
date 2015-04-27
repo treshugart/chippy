6fad6419760d5012c23768190c930fdb33fe249c
Trey Shugart <treshugart@gmail.com>
2015-04-28 09:46:36 +1000

    Abstract dist task.

--- src/task/dist.js
+++ src/task/dist.js
@@ -5,19 +5,20 @@ var galvatron = require('galvatron')();
 var gulp = require('gulp');
 var gulpRename = require('gulp-rename');
 var gulpUglify = require('gulp-uglify');
+var pkg = require('../lib/package');
 
 galvatron.transformer
   .post('babel')
   .post('globalize');
 
 module.exports = function () {
-  var bundle = galvatron.bundle('src/skate.js');
+  var bundle = galvatron.bundle('src/index.js');
   return gulp
     .src(bundle.files)
     .pipe(bundle.watchIf(commander.watch))
     .pipe(bundle.stream())
     .pipe(gulp.dest('dist'))
     .pipe(gulpUglify())
-    .pipe(gulpRename( { basename: 'skate.min' }))
+    .pipe(gulpRename( { basename: pkg.name + '.min' }))
     .pipe(gulp.dest('dist'));
 };

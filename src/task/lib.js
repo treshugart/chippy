ee58e36297eca1d57944c7279a4709b17bb370b0
Trey Shugart <treshugart@gmail.com>
2015-05-04 11:32:21 +1000

    Update lib task to remove the directory and then recreate it.

--- src/task/lib.js
+++ src/task/lib.js
@@ -1,23 +1,36 @@
 'use strict';
 
+var del = require('del');
 var galvatron = require('galvatron')();
 var gulp = require('gulp');
+var mac = require('mac');
 var path = require('path');
+
+var cwd = process.cwd();
+var src = path.join(cwd, 'src');
+var lib = path.join(cwd, 'lib');
 
 galvatron.transformer.post('babel', {
   modules: 'umd'
 });
 
-module.exports = function () {
-  var bundle = galvatron.bundle('src/index.js');
-  var src = path.join(process.cwd(), 'src');
-  bundle.all.forEach(function (file) {
-    var srcFile = path.relative(src, file);
-    var destFile = path.join('lib', srcFile);
-    var destDir = path.dirname(destFile);
-    gulp
-      .src(file)
-      .pipe(bundle.streamOne())
-      .pipe(gulp.dest(destDir));
-  });
-};
+module.exports = mac.series(
+  function (done) {
+    del(lib, done);
+  },
+
+  function () {
+    var bundle = galvatron.bundle('src/index.js');
+
+    bundle.all.forEach(function (file) {
+      var srcFile = path.relative(src, file);
+      var destFile = path.join('lib', srcFile);
+      var destDir = path.dirname(destFile);
+
+      gulp
+        .src(file)
+        .pipe(bundle.streamOne())
+        .pipe(gulp.dest(destDir));
+    });
+  }
+)
